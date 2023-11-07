bcaaedf0caf81b61d67fdf6416ff80e1956cea9b
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-11-08 00:31:02 +0400

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,25 +1,58 @@
 import sideBarImageForMobile from "./../images/bg-sidebar-mobile.svg";
+import sideBarImageForDesktop from "./../images/bg-sidebar-desktop.svg";
+
+interface StepData {
+  stepNumber: number;
+  stepTitle: string;
+  stepInfo: string;
+}
+
+const stepsData: StepData[] = [
+  { stepNumber: 1, stepTitle: "STEP 1", stepInfo: "YOUR INFO" },
+  { stepNumber: 2, stepTitle: "STEP 2", stepInfo: "SELECT PLAN" },
+  { stepNumber: 3, stepTitle: "STEP 3", stepInfo: "ADD-ONS" },
+  { stepNumber: 4, stepTitle: "STEP 4", stepInfo: "SUMMARY" },
+];
 
 const Sidebar = () => {
   return (
-    <div className="relative">
+    <div className="relative z-10 mx-auto">
       {/* Images */}
-      <div>
+      <div className="">
         <img
           src={sideBarImageForMobile}
           alt="sideBarImageForMobile"
-          className="w-full"
+          className="w-full flex md:hidden"
         />
+        <div className="bg-white p-4 rounded-l-[10px] hidden md:flex">
+          <img
+            src={sideBarImageForDesktop}
+            alt="sideBarImageForMobile"
+            className="w-full h-[568px] hidden md:flex rounded-[10px]"
+          />
+        </div>
       </div>
       {/* Buttons */}
       <div
-        className="absolute top-8 flex flex-row items-center gap-4 text-white text-sm
-      font-bold tracking-[1px] justify-center w-full leading-normal"
+        className="z-10 absolute mx-auto top-8 md:top-10 flex flex-row md:flex-col md:pl-8 md:pt-10
+      md:items-start md:left-8 items-center gap-4 text-white text-sm font-bold tracking-[1px]
+      justify-center w-full leading-normal"
       >
-        <button className="border rounded-full w-8 h-8 items-center">1</button>
-        <button className="border rounded-full w-8 h-8 items-center">2</button>
-        <button className="border rounded-full w-8 h-8 items-center">3</button>
-        <button className="border rounded-full w-8 h-8 items-center">4</button>
+        {stepsData.map((step) => (
+          <div key={step.stepNumber} className="flex flex-row gap-4">
+            <button className="border rounded-full w-8 h-8 items-center">
+              {step.stepNumber}
+            </button>
+            <div className="hidden md:flex flex-col">
+              <h2 className="text-[#ABBCFF] text-xs leading-normal">
+                {step.stepTitle}
+              </h2>
+              <h1 className="text-white text-sm font-bold tracking-[1px] uppercase">
+                {step.stepInfo}
+              </h1>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
