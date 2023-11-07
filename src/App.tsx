c116f2e7b600a50ecb799bd3397280d2292dd7a5
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-11-08 00:30:58 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import PersonalInfo from "./components/PersonalInfo";
 import Sidebar from "./components/Sidebar";
 
 const App = () => {
   return (
-    <div className="w-full h-screen bg-[#EFF5FF] flex flex-col md:justify-center md:items-center">
-      <Sidebar />
+    <div className="w-full h-screen bg-[#EFF5FF] flex flex-col md:flex-row md:justify-center md:items-center">
+      <div className="flex flex-row items-start justify-start">
+        <Sidebar />
+        <PersonalInfo />
+      </div>
     </div>
   );
 };
