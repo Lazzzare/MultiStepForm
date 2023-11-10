bd45533f4ad468264465efc76d4644b55066a51d
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-11-10 18:36:34 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,5 @@
-import { useState } from "react";
-import PersonalInfo from "./components/PersonalInfo";
-import SelectPlan from "./components/SelectPlan";
-import Sidebar from "./components/Sidebar";
-
 const App = () => {
-  const [isFormValid, setIsFormValid] = useState<boolean>(false);
-
-  return (
-    <div className="w-full h-screen bg-[#EFF5FF] flex flex-col md:flex-row md:justify-center md:items-center">
-      <div className="flex flex-row items-start justify-start">
-        <Sidebar />
-        <PersonalInfo setIsFormValid={setIsFormValid} />
-        <SelectPlan />
-      </div>
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
